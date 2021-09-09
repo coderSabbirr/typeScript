82a33145514af1f487cc59deda61ec8114b290c1
Nahid Hasan Sabbir <sabbirnahid5817@gmail.com>
2021-09-10 00:58:51 +0600

    type error

---
+++ array.ts
@@ -0,0 +1,29 @@
+const salary:number = 7500
+const friendSalaries:number[] =[7500, 12300, 45000, 8600]
+const friends:string[] =['sakib', 'rafin', 'nakib','tasfiya']
+
+friendSalaries[0] = 10500;
+friendSalaries.push(12000);
+
+// friendSalaries[3] ='1000'
+// friendSalaries.push('12330')
+
+
+friends[0] ='rakib';
+friends.push('robin')
+
+// friends.push(15000);
+
+// let max = "0";
+// for (const salary of friendSalaries) {
+//     if(salary > max){
+        
+//     }
+// }
+
+let max = 0;
+for (const salary of friendSalaries) {
+    if(salary > max){
+
+    }
+}
