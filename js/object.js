9ce6c8653dedfdb809ae5042f17ec66c66201fcc
Nahid Hasan Sabbir <sabbirnahid5817@gmail.com>
2021-09-10 01:31:00 +0600

    arrow function

---
+++ js/object.js
@@ -0,0 +1,13 @@
+"use strict";
+const city = 'Dhaka';
+const fruits = ['mango', 'banna', 'apple'];
+const student = {
+    name: 'Johir ali',
+    age: 25,
+    salary: 25000
+};
+const employee = {
+    name: 'Johir ali',
+    age: 25,
+    salary: 25000
+};
---
+++ object.ts
@@ -0,0 +1,20 @@
+const city: string ='Dhaka'
+const fruits: string [] = ['mango', 'banna', 'apple'];
+
+type Person = {
+    name: string,
+    age: number,
+    salary:number
+} 
+
+const student: Person = {
+     name:'Johir ali',
+     age:25,
+     salary:25000  
+}
+
+const employee: { name: string, age: number, salary: number} ={
+     name:'Johir ali',
+     age:25,
+     salary:25000  
+}
